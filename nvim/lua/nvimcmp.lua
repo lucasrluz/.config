db33921c1e606ae82ca85b35cb962a440deb7cd3
lucasrluz <lucasr.luz101@gmail.com>
2023-10-21 16:35:55 -0300

    refactor (neovim): alteração do atalho para navegação no nvim-cmp

--- nvim/lua/nvimcmp.lua
+++ nvim/lua/nvimcmp.lua
@@ -9,30 +9,9 @@ cmp.setup({
 
   	mapping = {
 		['<C-Space>'] = cmp.mapping.complete(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }),
-
-    	["<Tab>"] = cmp.mapping(function(fallback)
-      		if cmp.visible() then
-        		cmp.select_next_item()
-      		elseif luasnip.expand_or_jumpable() then
-        		luasnip.expand_or_jump()
-      		elseif has_words_before() then
-        		cmp.complete()
-      		else
-        		fallback()
-      		end
-    	end, { "i", "s" }),
-
-    	["<S-Tab>"] = cmp.mapping(function(fallback)
-      		if cmp.visible() then
-        		cmp.select_prev_item()
-      		elseif luasnip.jumpable(-1) then
-        		luasnip.jump(-1)
-      		else
-        		fallback()
-      		end
-    	end, { "i", "s" }),
-
+		['<CR>'] = cmp.mapping.confirm({ select = true }),	
+		["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+    	["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
   	},
   	
 	sources = cmp.config.sources({
